64b7c9764d1734faa1936a08ef37584eca9f901b
DolphiN1000 <88268032+DolphiN1000@users.noreply.github.com>
2022-09-26 21:47:14 +0300

    js

---
+++ index.js
@@ -0,0 +1,11 @@
+function calculateTotal(number) {
+    // Change code below this line
+    let total = 0;
+     for (let i = 1; i <= number.length; i +=1) {
+     total += number;
+   }
+   return total;
+     // Change code above this line
+   }
+
+   console.log(number(1));
