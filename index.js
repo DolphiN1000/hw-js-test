e35db66bd7a1f4512d714b2f93b940e4e139d950
DolphiN1000 <88268032+DolphiN1000@users.noreply.github.com>
2022-09-27 22:24:40 +0300

    test

--- index.js
+++ index.js
@@ -14,21 +14,62 @@
 //    console.log(calculateTotal(30));
 //    console.log(calculateTotal(10));
 
-function findLongestWord(string) {
-  // Change code below this line
-  const words = string.split(" ");
-  let word = words[0];
-  const wordsCount = words.length;
-  for (i = 0; i < wordsCount; i += 1) {
-    if (word.length < words[i].length) {
-      word = words[i];
-      // console.log(words[i]);      
-    }
+// function findLongestWord(string) {
+//   // Change code below this line
+//   const words = string.split(" ");
+//   let word = words[0];
+//   const wordsCount = words.length;
+//   for (i = 0; i < wordsCount; i += 1) {
+//     if (word.length < words[i].length) {
+//       word = words[i];
+//       // console.log(words[i]);      
+//     }
    
+//   }
+//  return word;
+//   // Change code above this line
+// }
+// console.log(findLongestWord("The quick brown fox jumped over the lazy dog"));
+// console.log(findLongestWord("Google do a roll"));
+// console.log(findLongestWord("May the force be with you"));
+
+// function calculateTotalPrice(order) {
+//   let total = 0;
+//   // Change code below this line
+// for (let i of order)
+//  {
+//     total += order[i];
+//   }
+
+//   // Change code above this line
+//   return total;
+// }
+
+
+// function calculateTotalPrice(order) {
+//   let total = 0;
+//   // Change code below this line
+
+//   for (let i = 0; i < order.length; i += 1) {
+//     total += order[i];
+//   }
+
+//   // Change code above this line
+//   return total;
+// }
+
+function calculateTotalPrice(order) {
+  let total = 0;
+  // Change code below this line
+
+  for (const i of order) {
+    total += i;
   }
- return word;
+
   // Change code above this line
+  return total;
 }
-console.log(findLongestWord("The quick brown fox jumped over the lazy dog"));
-console.log(findLongestWord("Google do a roll"));
-console.log(findLongestWord("May the force be with you"));
+
+console.log(calculateTotalPrice([12, 85, 37, 4]));
+console.log(calculateTotalPrice([412, 371, 94, 63, 176]));
+console.log(calculateTotalPrice([0]));
