baa0ece51c2098d70af8d9f1f1afdb707c0d15ed
DolphiN1000 <88268032+DolphiN1000@users.noreply.github.com>
2022-09-28 17:43:12 +0300

    test

--- index.js
+++ index.js
@@ -155,16 +155,42 @@
 // bookShelf.removeBook("The Mist");
 // console.log(bookShelf.getBooks()); //
 
-const book = {
-  title: "The Last Kingdom",
-  author: "Bernard Cornwell",
-  genres: ["historical prose", "adventure"],
-  rating: 8.38,
+// const book = {
+//   title: "The Last Kingdom",
+//   author: "Bernard Cornwell",
+//   genres: ["historical prose", "adventure"],
+//   rating: 8.38,
+// };
+
+// for (const key in book) {
+//   // Ключ
+//   console.log(key);
+//   // Значення властивості з таким ключем
+//   console.log(book[key]);
+// }
+
+
+const apartment = {
+  imgUrl: "https://via.placeholder.com/640x480",
+  descr: "Spacious apartment in the city center",
+  rating: 4,
+  price: 2153,
+  tags: ["premium", "promoted", "top"],
+  owner: {
+    name: "Henry",
+    phone: "982-126-1588",
+    email: "henry.carter@aptmail.com",
+  },
 };
 
-for (const key in book) {
-  // Ключ
-  console.log(key);
-  // Значення властивості з таким ключем
-  console.log(book[key]);
-}
+// Change code below this line
+const ownerName = apartment.owner.name;
+const ownerPhone = apartment.owner.phone;
+const ownerEmail = apartment.owner.email;
+const numberOfTags = apartment.tags;
+const firstTag = apartment.tags[0];
+const lastTag = apartment.tags[apartment.tags.length - 1];
+// Change code above this line
+
+
+
