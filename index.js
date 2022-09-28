abc5ac2b32eeb5ba1913fdfc63088968328f76b3
DolphiN1000 <88268032+DolphiN1000@users.noreply.github.com>
2022-09-28 19:12:46 +0300

    еуеі

--- index.js
+++ index.js
@@ -170,27 +170,45 @@
 // }
 
 
+// const apartment = {
+//   imgUrl: "https://via.placeholder.com/640x480",
+//   descr: "Spacious apartment in the city center",
+//   rating: 4,
+//   price: 2153,
+//   tags: ["premium", "promoted", "top"],
+//   owner: {
+//     name: "Henry",
+//     phone: "982-126-1588",
+//     email: "henry.carter@aptmail.com",
+//   },
+// };
+
+// // Change code below this line
+// const ownerName = apartment.owner.name;
+// const ownerPhone = apartment.owner.phone;
+// const ownerEmail = apartment.owner.email;
+// const numberOfTags = apartment.tags.length;
+// const firstTag = apartment.tags[0];
+// const lastTag = apartment.tags[apartment.tags.length - 1];
+// // Change code above this line
+// console.log(numberOfTags)
+
 const apartment = {
   imgUrl: "https://via.placeholder.com/640x480",
   descr: "Spacious apartment in the city center",
-  rating: 4,
-  price: 2153,
-  tags: ["premium", "promoted", "top"],
+  rating: 4.7,
+  price: 5000,
+  tags: ["premium", "promoted", "top", "trusted"],
   owner: {
-    name: "Henry",
+    name: "Henry Sibola",
     phone: "982-126-1588",
     email: "henry.carter@aptmail.com",
   },
 };
 
 // Change code below this line
-const ownerName = apartment.owner.name;
-const ownerPhone = apartment.owner.phone;
-const ownerEmail = apartment.owner.email;
-const numberOfTags = apartment.tags;
-const firstTag = apartment.tags[0];
-const lastTag = apartment.tags[apartment.tags.length - 1];
-// Change code above this line
-
-
-
+apartment.area = 60;
+apartment.rooms = 3;
+apartment.location = '';
+apartment.location.country = "Jamaica";
+apartment.location.city = "Kingston";
